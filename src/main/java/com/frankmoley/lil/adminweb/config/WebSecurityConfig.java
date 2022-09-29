2fd1d2a97151dfa0568b5183e3216404576c6f4f
Aziz1019 <starinc1998@gmail.com>
2022-09-29 10:54:02 +0500

    Initial commit

---
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.frankmoley.lil.adminweb.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public UserDetailsService users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    // From the video it is said that the spring boot app should encode the password itself by
+    // default in bycrypt, but in our case it is not doing so.
+
+    // Let's watch the rest and see what happens.
+
+}
